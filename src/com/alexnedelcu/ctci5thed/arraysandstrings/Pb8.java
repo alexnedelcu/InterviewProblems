189074553c136a5d692441c6ea181460eb6610c8
Alexandru Nedelcu <ialexandru.nedelcu@gmail.com>
2015-11-30 15:27:35 -0500

    Problem 8 done in pseudocode

---
+++ src/com/alexnedelcu/ctci5thed/arraysandstrings/Pb8.java
@@ -0,0 +1,10 @@
+package com.alexnedelcu.ctci5thed.arraysandstrings;
+
+public class Pb8 {
+
+	public static void main(String[] args) {
+		// done in pseudocode:
+		// concatenate the strings s2 with s2, and then return isSubstring (s1, s2+s2)
+	}
+
+}
